c20be57dff28d42464c9c3f23d4d555be0d0a32d
Briseida Garcia Espinoza <201G0253@rcarbonifera.tecnm.mx>
2022-11-13 23:55:32 -0600

    Agregar archivos de proyecto.

---
+++ Graficacion/Casette.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Graficacion
+{
+    /// <summary>
+    /// Lógica de interacción para Casette.xaml
+    /// </summary>
+    public partial class Casette : Window
+    {
+        public Casette()
+        {
+            InitializeComponent();
+        }
+    }
+}
